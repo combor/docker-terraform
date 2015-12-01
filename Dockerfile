31216d8716a168062f5ed5358499fb56dc68e196
Piotr Komborski <piotr.komborski@gmail.com>
2015-12-01 13:29:40 +0000

    New tests

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ ENV PATH $PATH:/usr/local/bin
 ENV TERRAFORM_VER 0.6.7
 ENV TERRAFORM_ZIP terraform_${TERRAFORM_VER}_linux_amd64.zip
 
-RUN apt-get update && apt-get -y install wget unzip openssh-client dnsutils git pass ruby
+RUN apt-get update && apt-get -y install wget unzip openssh-client
 RUN set -ex \
        && wget https://releases.hashicorp.com/terraform/${TERRAFORM_VER}/${TERRAFORM_ZIP} -O /tmp/$TERRAFORM_ZIP 
 RUN unzip /tmp/$TERRAFORM_ZIP -d /usr/local/bin 
--- spec/specs.rb
+++ spec/specs.rb
@@ -1,13 +1,47 @@
 require 'docker'
-
-#Docker.url = "https://192.168.99.100:2376"
+require 'serverspec'
 
 describe "Terraform image" do
     before(:all) {
         @image = Docker::Image.all().detect{|i| i.info['RepoTags'].include? 'terraform:latest' }
+        set :os, family: :debian
+        set :backend, :docker
+        set :docker_image, @image.id
     }
     
     it "should be availble" do
         expect(@image).to_not be_nil
+    end
+
+    it "installs the right version of Ubuntu" do
+        expect(os_version).to include("Ubuntu 14")
+    end
+
+    def os_version
+        command("lsb_release -a").stdout
+    end
+
+    it "installs required packages" do
+        expect(package("unzip")).to be_installed
+    end
+
+    it "installs required packages" do
+        expect(package("openssh-client")).to be_installed
+    end
+
+    it "installs required packages" do
+        expect(package("wget")).to be_installed
+    end
+
+    it "checks if terraform binary is executable" do
+         expect(file("/usr/local/bin/terraform")).to be_mode 775
+    end
+
+    it "has the Terraform version 0.6.7" do
+        expect(terraform_version).to include("0.6.7")
+    end
+
+    def terraform_version
+        command("terraform version").stdout
     end
 end
